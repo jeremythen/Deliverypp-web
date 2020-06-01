b453d04b34786b7a5b8fbc4890a08c6e5d162527
Jeremy Then <32010457+jeremythen@users.noreply.github.com>
2020-05-31 22:39:39 -0400

    Removing expiration time for jtw token

--- src/main/java/com/deliverypp/security/JwtTokenProvider.java
+++ src/main/java/com/deliverypp/security/JwtTokenProvider.java
@@ -17,20 +17,20 @@ public class JwtTokenProvider {
     @Value("${app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${app.jwtExpirationInMs}")
-    private int jwtExpirationInMs;
+    //@Value("${app.jwtExpirationInMs}")
+    //private int jwtExpirationInMs;
 
     public String generateToken(Authentication authentication) {
 
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
         Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+        //Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
 
         return Jwts.builder()
                 .setSubject(Long.toString(userPrincipal.getId()))
                 .setIssuedAt(new Date())
-                .setExpiration(expiryDate)
+                //.setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
